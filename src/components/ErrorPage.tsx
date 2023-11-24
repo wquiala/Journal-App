a00a0829021588cc7443bf6a7910c111412b5feb
Wilfredo <wilfredoquiala@gmail.com>
2023-11-24 18:38:01 +0100

    Journal with MU

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,7 +1,7 @@
 export const ErrorPage = () => {
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
+      <h1>Oops</h1>
       <p>Sorry, an unexpected error has occurred.</p>
     </div>
   );
